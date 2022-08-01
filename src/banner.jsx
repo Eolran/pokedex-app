d16b0f6b4bf464c4c61c79b10d39c4e90e2e8b06
Eolran <nevadasmith147@gmail.com>
2022-08-01 11:15:20 +0100

    banner & search components added

---
+++ src/banner.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function Banner() {
+
+  return (
+    <nav className="Banner">
+      <button>
+        Accueil
+      </button>
+      <button>
+        Recherche
+      </button>
+    </nav>
+  )
+}
+
+export default Banner
