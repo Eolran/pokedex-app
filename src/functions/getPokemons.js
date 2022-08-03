91efc974fcc0402704e62a0a7f0ec79b388d911f
Eolran <nevadasmith147@gmail.com>
2022-08-03 14:59:58 +0100

    Update: no more pokemon example & auto find pokemon

--- src/functions/getPokemons.js
+++ src/functions/getPokemons.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export function usePokemons() {
-    const [pokemons, setPokemons] = useState(null);
+    const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
@@ -15,16 +15,30 @@ export function usePokemons() {
     return pokemons;
 }
 
-export function getPokemon() {
+export function getPokemon(id) {
     const [pokemon, setPokemon] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
-            const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+            const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/'+id);  
             setPokemon(data);
         }
         fetchData();
     }, []); 
 
     return pokemon;
+}
+
+export function getAbility() {
+    const [pokeAbility, setPokeAbility] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data } = await axios.get('https://pokeapi.co/api/v2/ability/65');
+            setPokeAbility(data);
+        }
+        fetchData();
+    }, []); 
+
+    return pokeAbility;
 }
