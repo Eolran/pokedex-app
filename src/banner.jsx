0dd2ec342cdb2daea1b97c8fd8b734112dc3c737
Eolran <nevadasmith147@gmail.com>
2022-08-03 16:36:17 +0100

    Update: Add route to homepage

--- src/banner.jsx
+++ src/banner.jsx
@@ -1,15 +1,20 @@
 import './App.css'
+import Search from './search'
+import {BrowserRouter, Link} from "react-router-dom";
 
 function Banner() {
 
   return (
     <nav className="Banner">
-      <button>
-        Accueil
-      </button>
-      <button>
-        Recherche
-      </button>
+      <BrowserRouter>
+        <Link to="/">
+          <button>
+            Accueil
+          </button>
+        </Link>
+      </BrowserRouter>
+      
+      <Search />
 
       
     </nav>
